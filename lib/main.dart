b4543548ee81c14f4cf1ef4895cfc1568b943663
Sanjay-2001 <sanjaydutta976@gmail.com>
2025-08-17 11:59:02 +0530

    FEAT: firebase setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,14 @@
 import 'package:firebase_chat_app/auth/login_or_register.dart';
+import 'package:firebase_chat_app/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_chat_app/themes/light_mode.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
   runApp(const MainApp());
 }
 
